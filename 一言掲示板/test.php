3d6337836c5e73ca64433987f8450ab4f12d7ebd
tech-kotaro <73010509+tech-kotaro@users.noreply.github.com>
2020-10-18 18:05:14 +0900

    Add files via upload

---
+++ 一言掲示板/test.php
@@ -0,0 +1,33 @@
+<?php
+
+// メッセージを保存するファイルのパス設定
+define( 'FILENAME', './message.txt');
+
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+// 変数の初期化
+$now_date = null;
+$data = null;
+$file_handle = null;
+$split_data = null;
+$message = array();
+$message_array = array();
+
+if( $file_handle = fopen( FILENAME,'r') ) {
+    while( $data = fgets($file_handle) ){
+
+        $split_data = preg_split( '/\'/', $data);
+
+        $message = array(
+            'view_name' => $split_data[1],
+            'message' => $split_data[3],
+            'post_date' => $split_data[5]
+        );
+        array_unshift( $message_array, $message);
+    }
+
+    // ファイルを閉じる
+    fclose( $file_handle);
+}
+?>
